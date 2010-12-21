0725c18c5d85050741e2289c561b852ae8d79ccf
julien.lespagnard@gmail.com <julien.lespagnard@gmail.com@d7cfa61b-a3f8-406e-a8db-a75ced185756>
2010-12-21 19:45:42 +0000

    Ajout de la validation du cricuit : il doit être fermé et comporter un élément départ. Ajout de fonctionnalités dans le menu pour changer la taile de la grille et nettoyer la grille.

--- trunk/src/fr/unice/miage/vroomaniacs/circuit/Circuit.java
+++ trunk/src/fr/unice/miage/vroomaniacs/circuit/Circuit.java
@@ -1,5 +1,6 @@
 package fr.unice.miage.vroomaniacs.circuit;
 
+import java.awt.Point;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -9,6 +10,13 @@ import java.util.Map;
 
 import fr.unice.miage.vroomaniacs.circuit.element.Element;
 import fr.unice.miage.vroomaniacs.circuit.element.RouteDepart;
+import fr.unice.miage.vroomaniacs.circuit.element.RouteH;
+import fr.unice.miage.vroomaniacs.circuit.element.RouteV;
+import fr.unice.miage.vroomaniacs.circuit.element.VirageID;
+import fr.unice.miage.vroomaniacs.circuit.element.VirageIG;
+import fr.unice.miage.vroomaniacs.circuit.element.VirageSD;
+import fr.unice.miage.vroomaniacs.circuit.element.VirageSG;
+import fr.unice.miage.vroomaniacs.utils.Utils;
 
 /**
  * @author Julien Lespagnard
@@ -88,6 +96,112 @@ public class Circuit implements Iterable<Element> {
 		for(String id : ids) {
 			this.m_elements.put(id, newElements.get(id));
 		}
+	}
+	
+	/**
+	 * Nettoie la liste des &eacute;l&eacute;ments du cricuit.
+	 */
+	public void clearElements() {
+		this.m_elements.clear();
+	}
+	
+	/**
+	 * @return	<code>true</code> si le circuit est valide (ferm&eacute; avec un d&eacute;part), <code>false</code> sinon
+	 */
+	public boolean estValide() {
+		if(this.m_idElementDepart == null) {
+			return false;
+		}
+		
+		boolean estValide = false;
+		Point ouest,nord,est,sud;
+		for(Element elem : this) {
+			if(estElementRouteOuVirage(elem)) {
+				ouest = Utils.getPointOuest(elem);
+				if(ouest != null) {
+					estValide = this.testConnexion(elem, ouest);
+				}
+				if(estValide) {
+					nord = Utils.getPointNord(elem);
+					if(nord != null) {
+						estValide = this.testConnexion(elem, nord);
+					}
+				}
+				if(estValide) {
+					est = Utils.getPointEst(elem);
+					if(est != null) {
+						estValide = this.testConnexion(elem, est);
+					}
+				}
+				if(estValide) {
+					sud = Utils.getPointSud(elem);
+					if(sud != null) {
+						estValide = this.testConnexion(elem, sud);
+					}
+				}
+			}
+		}
+		return estValide;
+	}
+	
+	/**
+	 * @param p_elem
+	 * @param p_point
+	 * @return	<code>true</code> si une connexion existe entre un &eacute;l&eacute;ment du circuit 
+	 * 			<code>!= p_elem</code> et le point <code>p_point</code>, <code>false</code> sinon
+	 */
+	private boolean testConnexion(Element p_elem, Point p_point) {
+		Iterator<Element> itElements = this.iterator();
+		Point[] pointsElem;
+		Element elem2;
+		while(itElements.hasNext()) {
+			elem2 = itElements.next();
+			
+			if(estElementRouteOuVirage(elem2) && (elem2 != p_elem)) {
+				pointsElem = new Point[]{Utils.getPointOuest(elem2),Utils.getPointNord(elem2),Utils.getPointEst(elem2),Utils.getPointSud(elem2)};
+				
+				if(this.estConnecte(p_point, pointsElem)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @param p_element
+	 * @return	<code>true</code> si l'&eacute;l&eacute;ment est une route ou un virage, <code>false</code> sinon
+	 */
+	private boolean estElementRouteOuVirage(Element p_element) {
+		if(p_element instanceof RouteDepart)
+			return true;
+		if(p_element instanceof RouteH)
+			return true;
+		if(p_element instanceof RouteV)
+			return true;
+		if(p_element instanceof VirageIG)
+			return true;
+		if(p_element instanceof VirageSG)
+			return true;
+		if(p_element instanceof VirageSD)
+			return true;
+		if(p_element instanceof VirageID)
+			return true;
+		return false;
+	}
+	
+	/**
+	 * @param p_point
+	 * @param p_points
+	 * @return	<code>true</code> si le point <code>p_point</code> a une connexion avec un des points pr&eacute;sents dans <code>p_points</code>
+	 */
+	private boolean estConnecte(Point p_point, Point[] p_points) {
+		for(Point point : p_points) {
+			if(p_point.equals(point)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	@Override
