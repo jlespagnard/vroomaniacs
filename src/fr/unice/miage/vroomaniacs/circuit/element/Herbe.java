689fa1205429591545a9f22714cfa0960bad3f7d
julien.lespagnard@gmail.com <julien.lespagnard@gmail.com@d7cfa61b-a3f8-406e-a8db-a75ced185756>
2011-01-02 15:32:35 +0000

    Finalisation de la sauvegarde et du chargement d'un circuit. => Sérialisation/déserialisation d'un circuit DONE !
    Mise des items à placer dans le cricuit au sein d'un scrollpane pour gérer les débordements.

--- src/fr/unice/miage/vroomaniacs/circuit/element/Herbe.java
+++ src/fr/unice/miage/vroomaniacs/circuit/element/Herbe.java
@@ -9,8 +9,9 @@ import fr.unice.miage.vroomaniacs.circuit.gui.EditeurCircuit;
  * @author Julien Lespagnard
  * @version	1.0
  */
-@SuppressWarnings("serial")
 public class Herbe extends Element implements IDecor, Serializable {
+	private static final long serialVersionUID = 2L;
+	
 	public Herbe(EditeurCircuit p_editeur, Builder p_builder, String p_id) {
 		super(p_editeur, p_builder, p_id);
 	}
