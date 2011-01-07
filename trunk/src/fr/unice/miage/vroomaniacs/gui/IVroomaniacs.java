0af3a76a1b95636fc71a82bdebc8a052eada5583
e.chronos <e.chronos@d7cfa61b-a3f8-406e-a8db-a75ced185756>
2011-01-07 12:47:39 +0000

    Ajout de l'interface IVroomaniacs et modifications en conséquence de Vroomaniacs.java

---
+++ trunk/src/fr/unice/miage/vroomaniacs/gui/IVroomaniacs.java
@@ -0,0 +1,10 @@
+package fr.unice.miage.vroomaniacs.gui;
+
+import java.util.*;
+
+import fr.unice.miage.vroomaniacs_plugins.pluginsSDK.Dessinable;
+
+public interface IVroomaniacs {
+	public List<Dessinable> getListeObjetDessinable();
+
+}
