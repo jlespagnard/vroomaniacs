7857b10df0c65dba0ca480aa3da7e18dad3a16a8
michel.cartier83@gmail.com <michel.cartier83@gmail.com@d7cfa61b-a3f8-406e-a8db-a75ced185756>
2011-01-08 20:22:09 +0000

    Modification du ComportementArretStand

--- trunk_plugin/src/fr/unice/miage/vroomaniacs_plugins/comportements/ComportementArretStand.java
+++ trunk_plugin/src/fr/unice/miage/vroomaniacs_plugins/comportements/ComportementArretStand.java
@@ -1,93 +1,103 @@
 package fr.unice.miage.vroomaniacs_plugins.comportements;
 
+
+
 import java.awt.Point;
 import java.util.List;
 
-import fr.unice.miage.vroomaniacs_plugins.pluginsSDK.ComportementPlugin;
+import fr.unice.miage.vroomaniacs_plugins.pluginsSDK.IVroomaniacs;
 import fr.unice.miage.vroomaniacs_plugins.pluginsSDK.ObjetAnimePlugin;
-import fr.unice.miage.vroomaniacs_plugins.pluginsSDK.Utils;
 
-public class ComportementArretStand extends ComportementSuitChemin
-{
-	private long tempStand;
+
+
+public class ComportementArretStand extends ComportementSuitChemin{
+	private List<Point> chem;
+	private Point stand;
+	private int lastPoint = 0;
 	private long tempPrec;
-    
-	public ComportementArretStand() {
-		super();
+	private long tempStand;
+	private boolean firstTime = false;
+	private boolean active = true;
+	public ComportementArretStand(List<Point> che,Point p_stand) {
+		super(che);
+		chem = che;
+		stand = p_stand;
 	}
-	
-    public ComportementArretStand(List<Point> chemin)
-    {
-    	super(chemin);
-    	tempStand = 0;
-    	tempPrec = 0;
-    }
-    
-	@Override
-	public boolean canProcess(Object arg0) {
-		return true;
+	public ComportementArretStand(IVroomaniacs p_vroomaniacs)
+	{
+		stand = p_vroomaniacs.getStand();
 	}
-
-	@Override
-	public String getDescription() {
-		return "Comportement Arret Stand, pour qu'une voiture s'arrete au stand";
+	public void setActive(boolean bool)
+	{
+		active = bool;
 	}
-
-	@Override
-	public String getName() {
-		return "Comportement Stand";
-	}
-
-	@Override
-	public Class getType() {
-		return ComportementArretStand.class;
-	}
-
-	@Override
-	public String getVersion() {
-		return "1.0";
-	}
-
-	@Override
-	public boolean matches(Class arg0, String arg1, Object arg2) {
-		return true;
-	}
-
-	@Override
-	public void deplace(ObjetAnimePlugin o) {
-		Point stand = o.getJeu().getStand();
-		if(!o.getJeu().getStand().equals(null) && (Utils.distance(o.getXPos(), o.getYPos(), stand.x,stand.y)<Utils.getLargeurRoute()*2))
+	public void suitChemin(ObjetAnimePlugin o)
+	{
+		if(lastPoint < chem.size())
 		{
-				o.setDirection(Math.atan2(stand.y, stand.x));
-				if((o.getXPos() == stand.x) && (o.getYPos() == stand.y))
-				{
-					long time = System.currentTimeMillis();
-					tempStand += time-tempPrec;
-					tempPrec = time;
-					if(tempStand > 5000)
-					{
-						//to do remise sur le circuit
-						
-					}
-				}
+			double dx = chem.get(lastPoint).x - o.getXPos();
+            double dy = chem.get(lastPoint).y - o.getYPos();
+            o.setDirection(Math.atan2(dy, dx));
+            o.normaliseDirection();
+            o.setXPos(o.getXPos()+(o.getVitesse()*Math.cos(o.getDirection())));
+	        o.setYPos(o.getYPos()+(o.getVitesse()*Math.sin(o.getDirection())));
+            
+			if(fr.unice.miage.vroomaniacs_plugins.pluginsSDK.Utils.distance(o.getXPos(),o.getYPos(), chem.get(lastPoint).x, chem.get(lastPoint).y)< 20)
+			{
+				lastPoint ++;
+			}
 		}
 		else
 		{
-			// On recupere les coordonnees du prochain point de passage
-	        Point p = chemin.get(indicePointCourant);
-	
-	        // Si on est trop pres du point courant, on passe au point suivant
-	        if (Utils.distance(o.getXPos(), o.getYPos(), p.x, p.y) < distanceValidationPassage) {
-	            // On passe au point suivant
-	            indicePointCourant++;
-	            indicePointCourant %= chemin.size();
-	        }
-	
-	        // Nouvelle direction a suivre
-	        double dx = p.x - o.getXPos();
-	        double dy = p.y - o.getYPos();
-	        o.setDirection(Math.atan2(dy, dx));
-	        o.normaliseDirection();
+			lastPoint = 0;
+		}
+	}
+	public void suitStand(ObjetAnimePlugin o)
+	{
+		double xtg = stand.x  - o.getXPos();
+		double ytg = stand.y - o.getYPos();
+		o.setDirection(Math.atan2(ytg, xtg));
+		o.setXPos(o.getXPos()+(o.getVitesse()*Math.cos(o.getDirection())));
+	    o.setYPos(o.getYPos()+(o.getVitesse()*Math.sin(o.getDirection())));
+        //o.vitesse = o.vitesse + o.accelerationLineaire;//en direction du stand
+	}
+	@Override
+	public void deplace(ObjetAnimePlugin o)
+	{
+		if(fr.unice.miage.vroomaniacs_plugins.pluginsSDK.Utils.distance(o.getXPos(), o.getYPos(), stand.x,stand.y)<30)
+		{//si le stand est renseigné, que la distance est sufisante, et que le comportment doit se faire
+			if(fr.unice.miage.vroomaniacs_plugins.pluginsSDK.Utils.distance(o.getXPos(), o.getYPos(), stand.x,stand.y)<10 && active)
+			{
+				long time = System.currentTimeMillis();
+				if(!firstTime)
+				{
+					tempPrec = time;
+				}
+				tempStand= time - tempPrec;
+				firstTime = true;
+				if(tempStand >= 5*1000)
+				{
+					System.out.println("reprise");
+					suitChemin(o);
+					active = false;
+				}
+			}
+			else if(active)
+			{
+				suitStand(o);
+			}
+			else
+			{
+				suitChemin(o);
+				firstTime = false;
+				tempStand=0;
+				tempPrec=0;
+			}
+		}
+		else
+		{
+			suitChemin(o);
+			active = true;
 		}
 	}
 }
